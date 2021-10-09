836832397c112e83862cfdf0593221a0e2dc5f4f
Radityaan <48042121+Radityaan@users.noreply.github.com>
2021-10-09 08:47:01 +0700

    Add files via upload

--- tugas1to/algenknepsek.php
+++ tugas1to/algenknepsek.php
@@ -1,66 +1,193 @@
 <?php
 
+class Parameters
+{
+    const FILE_NAME = 'products.txt';
+    const COLUMNS = ['item', 'price'];
+    const PUPULATION_SIZE = 10;
+    const BUDGET = 280000;
+    const STOPPING_VALUE = 10000;
+}
 class Catalogue
 {
-    function createProductColumn($column, $listOfRawProduct){
+    function createProductColumn($listOfRawProduct){
         foreach (array_keys($listOfRawProduct) as $listOfRawProductKey){
-            $listOfRawProduct[$columns[$listOfRawProductKey]] = $listOfRawProduct[$listOfRawProductKey];
+            $listOfRawProduct[Parameters::COLUMNS[$listOfRawProductKey]] = $listOfRawProduct[$listOfRawProductKey];
             unset($listOfRawProduct[$listOfRawProductKey]);
         }
 
     }
-    function product($parameters){
+    function product(){
         $collectionOfListProduct = [];
 
-        $raw_data = file($parameters['file_name']);
+        $raw_data = file(Parameters::FILE_NAME);
         foreach ($raw_data as $listOfRawProduct) {
-            $collectionOfListProduct[] = $this->createProductColumn($parameters['columns'], explode(",", $listOfRawProduct));
+            $collectionOfListProduct[] = $this->createProductColumn(explode(",", $listOfRawProduct));
         }
 
-        foreach ($collectionOfListProduct as $listOfRawProduct){
-            print_r($listOfRawProduct);
-            echo '<br>';
-        }
-        return [
-            'product' =>$collectionOfListProduct,
-            'gen_length' => count($collectionOfListProduct),
-        ];
+        return $collectionOfListProduct;
     }
 
 }
 
-class PopulationGenerator
+class Individu
 {
-    function createIndividu($parameters){
+    function countNumberOfGen()
+    {
         $catalogue = new Catalogue;
-        $lengthOfGen = $catalogue->product($parameters)['gen_length'];
-        for ($i = 0; $ <= $catalogue->product()['gen_length']-1; $i++){
-            $ret[] = rand(0,1);
+        return count($catalogue->product());
+    }
+    function createRandomIndividu()
+    {
+        echo $this->countNumberOfGen();exist();
+        //for ($i=0; $ <= $)
+    }
+}
+
+class Population
+{
+
+    function createPopulation(){
+        $individu = new Individu;
+        for ($i = 0; $ <= Parameters::POPULATION_SIZE-1; $i++){
+            $ret[] = $individu->createRandomIndividu();
+        }
+
+        return $ret;
+    
+    }
+}
+
+class Fitness
+{
+    function selectingItem()
+    {
+        $catalogue = new Catalogue;
+        foreach($individu as $individuKey => $binaryGen){
+            if ($binaryGen === 1){
+                $ret[] = [
+                    'selectedKey' => $individuKey,
+                    'selectedPrice' => $catalogue->product()[$individuKey]['price']
+                ];
+            }
         }
         return $ret;
+
     }
 
-    function createPopulation($parameters){
-        for ($i = 0; $ <= $parameters['population_size']; $i++){
-            $ret[] = $this->createIndividu($parameters);
-        }
-        foreach ($ret as $key => $val){
-            print_r($val);
+    function calculateFitnessValue($individu)
+    {
+        return array_sum(array_column($this->selectingItem($individu),'selectedPrice'));
+    }
+
+    function countSelectedItem($individu)
+    {
+        return count($this->selectingItem($individu));
+    }
+
+    function searchBestIndividu($fits, $maxItem, $numberOfIndividuHasMaxItem)
+    {
+        if ($numberOfIndividuHasMaxItem === 1){
+            $index = array_search($maxItem, array_column($fits, 'numberOfSelectedItem'));
             echo '<br>';
+            return($fits[$index]);        
+        } else{
+            foreach ($fits as $key => $val){
+                if ($val['numberOfSelectedItem'] === $maxItem){
+                    echo $key.' '.$val['fitnessValue'].'<br>';
+                    $ret[] = [
+                        'individuKey' => $key,
+                        'fitnessValue' => $val['fitnessValue']
+                    ];
+                }
+            }
+
+            if (count(array_unique(array_column($ret, 'fitnessValue')))=== 1)
+            $index = rand(0, count($ret) -1);
+        } else{
+            $max = max(array_column($ret, 'fitnessValue'));
+            $index = array_search($max, array_column($ret, 'fitnessValue'));
         }
-        print_r($ret);
+        echo 'Hasil';
+        return ($ret[$index]);
+    }
+
+    function isFound($fits)
+    {
+        $countedMaxItems = array_count_values(array_column($fits, 'numberOfSelectedItem'));
+        print_r($countedMaxItems);
+        echo '<br>';
+        $maxItem = max(array_keys($countedMaxItems));
+        echo $maxItem;
+        echo '<br>';
+        echo $countedMaxItems[$maxItem];
+        $numberOfIndividuHasMaxItem = $countedMaxItems[$maxItem];
+
+        $bestFitnessValue = $this->searchBestIndividu($fits, $maxItem, $numberOfIndividuHasMaxItem)['fitnessValue'];
+        echo '<br>';
+        echo '<br>Best Fitness Value: '.$bestFitnessValue;
+    
+        $residual = Parameters::BUDGET - $bestFitnessValue;
+        echo ' Residual: '.$residual;
+
+        if($residual <= Parameters::STOPPING_VALUE && $residual > 0){
+            return TRUE;
+        }
+    }
+
+
+    function isFit($fitnessValue)
+    {
+        if($fitnessValue <= Parameters::BUDGET){
+            return TRUE;
+        }
+    }
+
+    function fitnessEvaluation($population)
+    {
+        $catalogue = new Catalogue;
+        foreach ($population as $listOfIndividuKey => $listOfIndividu){
+            echo 'Individu-'. $listOfIndividuKey. '<br>';
+            foreach ($listOfIndividu as $individuKey => $binaryGen){
+                echo $binaryGen. '&nbsp;&nbsp;';
+                print_r($catalogue->product()[$individuKey]);
+                echo '<br>';
+            }
+            $fitnessValue = $this->calculateFitnessValue($listOfIndividu);
+            $numberOfSelectedItem = $this->countSelectedItem($listOfIndividu);
+            echo 'Max. Item: '.$numberOfSelectedItem;
+            echo 'Fitness Value: '.$fitnessValue;
+            if ($this->isFit($fitnessValue)){
+                echo ' (Fit)';
+                $fits[] = [
+                    'selectedIndividuKey' => $listOfIndividuKey,
+                    'numberOfSelectedItem' => $numberOfSelectedItem,
+                    'fitnessValue' => $fitnessValue
+                ];
+                print_r($fits);
+            } else{
+                echo ' (Not Fit)';
+            }
+            echo '<p>';
+        }
+
+        if ($this->isFound($fits)){
+            echo ' Found';
+        } else{
+            echo ' >> Next Generation';
+        }
+
     }
 }
 
-$parameters= [
-    'file_name' => 'products.txt',
-    'columns' => ['item', 'harga'],
-    'population_size' => 10
 
-];
 
-$katalog = new Catalogue;
-$katalog->product($parameters);
+$initialPopulation = new Population;
+$population = $initialPopulation->createRandomPopulation();
 
-$initalPopulation = new PopulationGenerator;
-$initalPopultaion->createPopulation($parameters);
+$fitness = new Fitness;
+$fitness->fitnessPopulation(InitialPopulation);
+
+print_r($population);
+//$individu = new Individu;
+//print_r($individu->createRandomIndividu());
